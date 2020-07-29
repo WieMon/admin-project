956e02fec510e73bcb2e5f675f68aee95c8712fd
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-07-29 15:09:12 +0200

    Add styling for menu on mobile Fix shortMenu function

--- js/script.js
+++ js/script.js
@@ -7,23 +7,19 @@ const sectionContainer = document.querySelector('.section-container');
 if (window.screen.width < 767) {
   menu.classList.add('collapsed');
   sectionContainer.classList.add('expanded');
-  //console.log('small');
 } else {
-    menu.classList.remove('collapsed');
-    menu.classList.remove('expanded');
-    //console.log('big');
-  }
+  menu.classList.remove('collapsed');
+  menu.classList.remove('expanded');
+}
 
-function shortModal(cur = menu.classList) {
-  if (cur == 'menu') {
+function shortMenu(current = menu.classList) {
+  if (current == 'menu') {
     menu.classList.add('collapsed');
     sectionContainer.classList.add('expanded');
-  //console.log('long menu');
   } else {
-      menu.classList.remove('collapsed');
-      menu.classList.remove('expanded');
-
-    }
+    menu.classList.remove('collapsed');
+    sectionContainer.classList.remove('expanded');
+  }
 }
 
 hamburger.addEventListener('click', function(e) {
