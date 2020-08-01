0561d2fe64f1217c2f539e147cfa1a41b55334d6
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-08-01 11:50:44 +0200

    Fix bem for buttons Add canvas chart

--- js/script.js
+++ js/script.js
@@ -26,3 +26,49 @@ hamburger.addEventListener('click', function(e) {
   e.preventDefault();
   shortMenu();
 })
+
+/*Chart*/
+var ctx = document.getElementById('myChart').getContext('2d');
+var Chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    datasets: [{
+      label: 'Signups',
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      data: [280, 150, 200, 280, 350, 330, 320, 180, 250, 270]
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [300, 140, 240, 220, 370, 100, 150, 200, 400, 280],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+      hidden: true,
+    }]
+  },
+  options: {
+
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 100,
+          min: 0,
+          max: 600,
+        },
+      }],
+      xAxes: [{
+        ticks: {
+          thickLength: 0,
+        }
+      }]
+    },
+  }
+});
